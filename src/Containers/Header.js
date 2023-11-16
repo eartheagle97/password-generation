8c18711466a529c66fdae8392b3aab7705625513
eartheagle97 <patelkairav97@gmail.com>
2023-11-15 17:55:34 -0800

    Initial Commit

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -4,10 +4,15 @@ import { FaGithub } from "react-icons/fa";
 const Header = () => {
   return (
     <header className="container mx-auto py-7">
-      <p className="flex justify-center items-center">
-        <FaGithub className="mr-2" />{" "}
-        <span className="text-sm">eartheagle97/</span>
-      </p>
+      <a
+        href="https://github.com/eartheagle97/password-generation"
+        target="_blank"
+      >
+        <p className="flex justify-center items-center">
+          <FaGithub className="mr-2" />{" "}
+          <span className="text-sm">eartheagle97/password-generation</span>
+        </p>
+      </a>
     </header>
   );
 };
